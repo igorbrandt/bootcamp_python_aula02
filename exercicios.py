49cca639d648566cbf924bacc24a7745ec46ea5a
igor <igor@clubedovalor.com.br>
2025-05-30 15:11:35 -0300

    adicionar exercicios strings

--- exercicios.py
+++ exercicios.py
@@ -46,14 +46,28 @@ print(f"A área do círculo é: {area}")
 # #### Strings (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+string = input("Escreva uma palavra: ")
+print(string.upper())
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+string = input("Escreva seu nome completo: ")
+print(string.lower())
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+string = input("Escreva uma frase: ")
+print(string.strip())
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+string = input("Digite uma data no formato 'dd/mm/aaaa': ")
+print(string.split("/"))
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+str_1 = input("Escreva uma palavra: ")
+str_2 = input("Escreva outra palavra: ")
+resultado = print(str_1 + str_2)
+print(resultado)
 
 # #### Booleanos (`bool`)
 
 # 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+
+
 # 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
 # 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
 # 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
